8480a771820bc8bde8299bafdf267aaff992b046
David Bernal <leo.bernal1946@gmail.com>
2024-03-21 22:36:54 +0100

    first commit

---
+++ src/main/java/com/dberna2/checkstyle/javacheckstyleexample/JavaCheckStyleExampleApplication.java
@@ -0,0 +1,13 @@
+package com.dberna2.checkstyle.javacheckstyleexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCheckStyleExampleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JavaCheckStyleExampleApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/dberna2/checkstyle/javacheckstyleexample/JavaCheckStyleExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dberna2.checkstyle.javacheckstyleexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCheckStyleExampleApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
